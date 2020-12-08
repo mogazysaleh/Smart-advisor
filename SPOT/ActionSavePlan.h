8ee57c7a104047b30327c56e3c632c79c9c87cde
Mohamed Saleh <mogazysaleh@gmail.com>
2020-12-08 17:17:53 +0200

    Prepares

--- SPOT/ActionSavePlan.h
+++ SPOT/ActionSavePlan.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "Actions/Action.h"
+#include <iostream>
+#include <string>
+
 
 class ActionSavePlan : public Action {
-	public:
+
+public:
 		ActionSavePlan(Registrar*);
 		bool virtual Execute();
 		virtual ~ActionSavePlan();
