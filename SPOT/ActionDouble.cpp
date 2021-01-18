8d8df103072a127ac495f7f49c508e753e6e68c4
MuhammadKhalid0 <s-muhammad.khalid@zewailcity.edu.eg>
2021-01-19 01:26:21 +0200

    SAD

--- SPOT/ActionDouble.cpp
+++ SPOT/ActionDouble.cpp
@@ -29,23 +29,23 @@ bool ActionDouble::Execute()
 		pGUI->GetUserAction("You have added the second major (Double Major) as : " + maj);
 		cout << "m";
 	}
-	//if (choose == "CONCENTRATION")
-	//{
-	//}
-	//pGUI->PrintMsg("Please enter the concentration no.");
-	//string con = pGUI->GetSrting();
-	//int conNo = stoi(con);
-	//StudyPlan* pS = pReg->getStudyPlay();
-	//int conNo1 = pS->getConcentration();
-	//if (conNo == conNo1)
-	//{
-	//	pGUI->GetUserAction("Error! you have taken this concentration already in your main choice");
-	//}
-	//else
-	//{
-	//	pS->setConcentration2(conNo);
-	//	pGUI->GetUserAction("Second concentration added ! Please add their courses manually using add course");
-	//}
+	if (choose == "CONCENTRATION")
+	{
+		pGUI->PrintMsg("Please enter the concentration no.");
+		string con = pGUI->GetSrting();
+		int conNo = stoi(con);
+		StudyPlan* pS = pReg->getStudyPlay();
+		int conNo1 = pS->getConcentration();
+		if (conNo == conNo1)
+		{
+			pGUI->GetUserAction("Error! you have taken this concentration already in your main choice");
+		}
+		else
+		{
+			pS->setConcentration2(conNo);
+			pGUI->GetUserAction("Second concentration added ! Please add their courses manually using add course");
+		}
+	}
 	return true;
 }
 
